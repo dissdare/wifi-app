d0a428b6b9a303d7f630de50813a21db24aaf8de
fanzhengwei <2507804989@qq.com>
2026-09-14 16:56:57 +0800

    执行默认命令前关闭app，执行后打开app

--- lib/commands.dart
+++ lib/commands.dart
@@ -16,11 +16,19 @@ class CommandDef {
 const List<CommandDef> presetCommands = [
   CommandDef(
     '查看ec20激活状态',
-    r'''resp=$(ec20 'AT+QCFG="usbnet"' 2>&1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'+QCFG: "usbnet",1'* ]]; then echo "已激活"; elif [[ $resp == *'+QCFG: "usbnet",'* ]]; then echo "未激活"; else echo "未知响应"; fi''',
+    r'''echo; systemctl stop app; resp=$(ec20 'AT+QCFG="usbnet"' 2>&1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'+QCFG: "usbnet",1'* ]]; then echo "已激活"; elif [[ $resp == *'+QCFG: "usbnet",'* ]]; then echo "未激活"; else echo "未知响应"; fi; systemctl start app; echo''',
   ),
   CommandDef(
     '激活ec20',
-    r'''resp=$(ec20 'AT+QCFG="usbnet",1' 2>&1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'recv msg:OK'* ]]; then echo "激活成功"; elif [[ $resp == *'ERROR'* ]]; then echo "激活失败"; else echo "未知响应"; fi''',
+    r'''echo; systemctl stop app; resp=$(ec20 'AT+QCFG="usbnet",1' 2>&1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'recv msg:OK'* ]]; then echo "激活成功"; elif [[ $resp == *'ERROR'* ]]; then echo "激活失败"; else echo "未知响应"; fi; systemctl start app; echo''',
+  ),
+  CommandDef(
+    '查看插卡状态',
+    r'''echo; systemctl stop app; resp=$(ec20 'AT+CPIN?' 2>&1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'READY'* ]]; then echo "识卡成功"; elif [[ $resp == *'ERROR'* ]]; then echo "识卡失败"; else echo "未知响应"; fi; systemctl start app; echo''',
+  ),
+  CommandDef(
+    '查看信号质量',
+    r'''echo; systemctl stop app; resp=$(ec20 'AT+CSQ' 2>&1); csq=$(printf '%s\n' "$resp" | grep -oP '\+CSQ:\s*\K[0-9]+' | head -n1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'+CSQ: 99'* ]]; then echo "信号有问题，请排查sim卡问题"; elif [ -n "$csq" ]; then echo "信号质量：$csq"; else echo "未知响应"; fi; systemctl start app; echo''',
   ),
   CommandDef('中断当前操作', '', interrupt: true),
 ];
