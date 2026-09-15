6b4d029527550af42ef1e4561bf36ccebe5f1f0e
fanzhengwei <2507804989@qq.com>
2026-09-15 14:44:38 +0800

    wifi连接增加联网检测

--- lib/commands.dart
+++ lib/commands.dart
@@ -30,5 +30,9 @@ const List<CommandDef> presetCommands = [
     '查看信号质量',
     r'''echo; systemctl stop app; resp=$(ec20 'AT+CSQ' 2>&1); csq=$(printf '%s\n' "$resp" | grep -oP '\+CSQ:\s*\K[0-9]+' | head -n1); if [[ $resp == *"The serial ports did not open correctly"* ]]; then echo "和ec20通信失败"; elif [[ $resp == *'+CSQ: 99'* ]]; then echo "信号有问题，请排查sim卡问题"; elif [ -n "$csq" ]; then echo "信号质量：$csq"; else echo "未知响应"; fi; systemctl start app; echo''',
   ),
+  CommandDef(
+    '联网检测',
+    r'''echo; ((ping -c5 -W2 -i1 www.baidu.com >/dev/null 2>&1 && echo OK) & (ping -c5 -W2 -i1 8.8.8.8 >/dev/null 2>&1 && echo OK) & wait ) | grep -q OK && echo "联网成功" || echo "联网失败"; echo''',
+  ),
   CommandDef('中断当前操作', '', interrupt: true),
 ];
